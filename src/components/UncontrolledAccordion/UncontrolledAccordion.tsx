a6d542187ab9f359b95bc57a2762045eab7813c1
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-19 19:42:11 +0300

    08 - callbacks 1 (UncontrolledAccordion)

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -11,8 +11,7 @@ function UncontrolledAccordion(props: AccordionPropsType) {
 
     return (
         <div>
-            <AccordionTitle title={props.titleValue}/>
-            <button onClick={() => setCollapsed(!collapsed)}>TOGGLE</button>
+            <AccordionTitle title={props.titleValue} onClick={() => setCollapsed(!collapsed)}/>
             {!collapsed && <AccordionBody/>}
         </div>
     )
@@ -20,11 +19,12 @@ function UncontrolledAccordion(props: AccordionPropsType) {
 
 type AccordionTitlePropsType = {
     title: string
+    onClick: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log('AccordionTitle rendering')
-    return <h3>{props.title}</h3>
+    return <h3 onClick={() => props.onClick()}>{props.title}</h3>
 }
 
 function AccordionBody() {
--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -8,24 +8,30 @@ export function UncontrolledRating(props: RatingPropsType) {
 
     return (
         <div>
-            <Star selected={value > 0} setValue={() => setValue(1)}/>
-            <Star selected={value > 1} setValue={() => setValue(2)}/>
-            <Star selected={value > 2} setValue={() => setValue(3)}/>
-            <Star selected={value > 3} setValue={() => setValue(4)}/>
-            <Star selected={value > 4} setValue={() => setValue(5)}/>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     )
 }
 
 type StarType = {
     selected: boolean
-    setValue: () => void
+    value: 0 | 1 | 2 | 3 | 4 | 5
+    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void
 }
 
 function Star(props: StarType) {
     console.log('Star rendering')
-    return <span onClick={props.setValue}>{props.selected ? <b>star </b> : "star "}</span>
+    return <span onClick={() => props.setValue(props.value)}>{props.selected ? <b>star </b> : "star "}</span>
 }
+
+
+
+
+
 
 
 // import React, {useState} from "react";
@@ -38,22 +44,23 @@ function Star(props: StarType) {
 //
 //     return (
 //         <div>
-//             <Star selected={value > 0} setValue={setValue} value={1}/>
-//             <Star selected={value > 1} setValue={setValue} value={2}/>
-//             <Star selected={value > 2} setValue={setValue} value={3}/>
-//             <Star selected={value > 3} setValue={setValue} value={4}/>
-//             <Star selected={value > 4} setValue={setValue} value={5}/>
+//             <Star selected={value > 0} setValue={() => setValue(1)}/>
+//             <Star selected={value > 1} setValue={() => setValue(2)}/>
+//             <Star selected={value > 2} setValue={() => setValue(3)}/>
+//             <Star selected={value > 3} setValue={() => setValue(4)}/>
+//             <Star selected={value > 4} setValue={() => setValue(5)}/>
 //         </div>
 //     )
 // }
 //
 // type StarType = {
 //     selected: boolean
-//     value: 0 | 1 | 2 | 3 | 4 | 5
-//     setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void
+//     setValue: () => void
 // }
 //
 // function Star(props: StarType) {
 //     console.log('Star rendering')
-//     return <span onClick={() => props.setValue(props.value)}>{props.selected ? <b>star </b> : "star "}</span>
+//     return <span onClick={props.setValue}>{props.selected ? <b>star </b> : "star "}</span>
 // }
+
+
