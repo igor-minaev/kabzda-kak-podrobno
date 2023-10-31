d3ef83a114c7dec9ec42381315fb60991759a71d
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-31 22:07:45 +0300

    10 - start 1. Storybook

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+    onChange: () => void
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log('UncontrolledAccordion rendering')
+    return (
+        <div>
+            <AccordionTitle onChange={props.onChange} title={props.titleValue}/>
+            {!props.collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    onChange: () => void
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log('AccordionTitle rendering')
+    return <h3 onClick={props.onChange}>{props.title}</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default Accordion;
+
+
+// import React from "react";
+//
+// type AccordionPropsType = {
+//     titleValue: string
+//     collapsed: boolean
+//     onClick: (collapsed: boolean) => void
+// }
+//
+// function Accordion(props: AccordionPropsType) {
+//     console.log('UncontrolledAccordion rendering')
+//     return (
+//         <div>
+//             <AccordionTitle onClick={() => props.onClick(!props.collapsed)} title={props.titleValue}/>
+//             {!props.collapsed && <AccordionBody/>}
+//         </div>
+//     )
+// }
+//
+// type AccordionTitlePropsType = {
+//     title: string
+//     onClick: () => void
+// }
+//
+// function AccordionTitle(props: AccordionTitlePropsType) {
+//     console.log('AccordionTitle rendering')
+//     return <h3 onClick={props.onClick}>{props.title}</h3>
+// }
+//
+// function AccordionBody() {
+//     console.log('AccordionBody rendering')
+//     return (
+//         <ul>
+//             <li>1</li>
+//             <li>2</li>
+//             <li>3</li>
+//         </ul>
+//     )
+// }
+//
+// export default Accordion;
