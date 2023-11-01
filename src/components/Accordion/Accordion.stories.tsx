dca40eb16c1b9aba2607beed27296a227a573877
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-01 18:05:09 +0300

    10 - 1. Storybook

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,31 +1,44 @@
 import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
 
 import Accordion from './Accordion';
 import {useState} from "react";
 
-const meta: Meta<typeof Accordion> = {
-    component: Accordion
-};
-
-export default meta;
-type Story = StoryObj<typeof Accordion>;
-
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-// export const CollapsedAccordion: Story = {
-//     render: () => <Accordion titleValue='Menu' collapsed={true} onChange={()=>{}}/>,
+// const meta: Meta<typeof Accordion> = {
+//     component: Accordion,
 // };
-
-// export const OpenedAccordion: Story = {
-//     render: () => <Accordion titleValue='Menu' collapsed={false} onChange={()=>{}}/>,
+// export default meta;
+// type Story = StoryObj<typeof Accordion>;
+//
+// /*
+//  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+//  * See https://storybook.js.org/docs/react/api/csf
+//  * to learn how to use render functions.
+//  */
+// export const FirstStory: Story = {
+//     args: {
+//         titleValue: 'Menu',
+//         collapsed: false,
+//         onChange: () => {
+//         }
+//     }
 // };
+// export const SecondStory: Story = {
+//     args: {
+//         titleValue: 'Menu',
+//         collapsed: true,
+//         onChange: () => {
+//         }
+//     }
+// };
+export default {
+    component: 'Accordion'
+}
+
+const onChangeHandler = action('onChange')
 
 export const CollapsedAccordion = () => {
-    return <Accordion titleValue='Collapsed Accordion' collapsed={true} onChange={() => {
-    }}/>
+    return <Accordion titleValue='Collapsed Accordion' collapsed={true} onChange={onChangeHandler}/>
 }
 
 export const OpenedAccordion = () => {
@@ -35,6 +48,7 @@ export const OpenedAccordion = () => {
 
 export const AccordionWithState = () => {
     const [collapsed, setCollapsed] = useState(false)
-    return <Accordion titleValue='Accordion with state' collapsed={collapsed} onChange={() => setCollapsed((!collapsed))}/>
+    return <Accordion titleValue='Accordion with state' collapsed={collapsed}
+                      onChange={() => setCollapsed((!collapsed))}/>
 }
 
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -23,7 +23,7 @@ type AccordionTitlePropsType = {
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log('AccordionTitle rendering')
-    return <h3 onClick={props.onChange}>{props.title}</h3>
+    return <h3 onClick={(e)=>props.onChange()}>{props.title}</h3>
 }
 
 function AccordionBody() {
